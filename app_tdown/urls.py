4603604302a8cbbbe9488dbfebd673357e9d2a69
Alan Wesely <alanwesely.22@gmail.com>
2024-03-03 18:45:26 -0300

    Commit_Aula 63

--- app_tdown/urls.py
+++ app_tdown/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'games'
 
 urlpatterns = [
-    path('', views.home, name="home"),     # Acessa a Tela Home
+    path('', views.home, name="home"),  # Acessa a Tela Home
+    path('buscar/liga/<int:liga_id>/', views.liga, name="buscar-liga"),
     path('cadastro-partida/<id>/', views.cadPartida, name="cadPartida"),  # Acessa Cad de Partida
     path('visualizar-partida/<id>/', views.viewPartida, name="viewPartida"),  # Acessa Vis Partida
     path('cadastro-jogada/', views.cadJogada, name="cadJogada"),    # Acessa Tela Cad de Jogadas
--- app_tdown/views.py
+++ app_tdown/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from utils.games.factory import make_game
 from .models import Partida
@@ -9,7 +10,22 @@ def home(request):
     partida = Partida.objects.all().order_by('-id')
     return render(request, 'app_tdown/pages/home.html', context={
         'games': partida,
+        'title': f'Home'
     })
+
+
+def liga(request, liga_id):
+    partida = get_list_or_404(
+        Partida.objects.filter(
+            divisoePartida__id=liga_id
+        ).order_by('-id')
+    )
+
+    return render(request, 'app_tdown/pages/home.html', context={
+        'games': partida,
+        'title': f'{partida[0].divisoePartida.nomeEDivisao} - Liga'
+    })
+
 
 
 def cadPartida(request, id):
