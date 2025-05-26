4dc1fb9a9a56d9b1e5a6ddc83d22b41738c47604
Alan Wesely <alanwesely.22@gmail.com>
2025-05-26 20:20:47 -0300

    subir_Comit_20250526_I

---
+++ app_tdown/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from app_tdown.models import Partida
+
+
+
+class CadastroJogosForm(forms.models):
+  
--- app_tdown/models.py
+++ app_tdown/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 
 # Create your models here.
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA MODALIDADE
+######################################################################################################################
 class Modalidade(models.Model):
     tipoModalidade = models.CharField(max_length=100)
 
     def __str__(self):
         return self.tipoModalidade
     
-
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA FEDERAÇÃO
+######################################################################################################################
 class Federacoes(models.Model):
     nomeAbrev = models.CharField(max_length=10)
     nomeFederacao = models.CharField(max_length=100)
@@ -16,7 +21,9 @@ class Federacoes(models.Model):
     def __str__(self):
         return self.nomeAbrev
 
-
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA TIME
+######################################################################################################################
 class Time(models.Model):
     nomeEquipe = models.CharField(max_length=100)
     nomeAbreviado = models.CharField(max_length=50)
@@ -29,7 +36,9 @@ class Time(models.Model):
     def __str__(self):
         return self.nomeAbreviado
 
-
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA LIGA
+######################################################################################################################
 class Liga(models.Model):
     nomeAbrevLiga = models.CharField(max_length=10)
     nomeLiga = models.CharField(max_length=100)
@@ -40,6 +49,9 @@ class Liga(models.Model):
     def __str__(self):
         return self.nomeAbrevLiga
 
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA DIVISÕES CAMPEONATO
+######################################################################################################################
 class DivisoesCampeonatos(models.Model):
     nomeEDivisao = models.CharField(max_length=100)
     liga = models.ForeignKey(
@@ -49,7 +61,9 @@ class DivisoesCampeonatos(models.Model):
     def __str__(self):
         return self.nomeEDivisao
 
-
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA PARTIDA
+######################################################################################################################
 class Partida(models.Model):
     nomePartida = models.CharField(max_length=300)
     timeCasa = models.ForeignKey(
@@ -75,7 +89,135 @@ class Partida(models.Model):
     )
     logoPartida = models.ImageField(upload_to='app_tdown/covers/logoPartida/', blank=True, default='')
 
-
-
     def __str__(self):
         return self.nomePartida
+    
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA JOGADA
+#########################################################################################################################
+class Jogada(models.Model):
+    partida = models.ForeignKey(Partida, on_delete=models.CASCADE)
+    jogada = models.IntegerField()
+    tempo = models.IntegerField()
+    descida = models.IntegerField()
+    timeAtacando = models.ForeignKey(Time, on_delete=models.SET_NULL, null=True, related_name='time_atacando')
+    timeDefendendo = models.ForeignKey(Time, on_delete=models.SET_NULL, null=True, related_name='time_defendendo')
+    jardaFirst = models.IntegerField()
+    posicaoBola = models.IntegerField()
+
+    def __str__(self):
+        return f"Jogada {self.jogada} - Tempo {self.tempo}"
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA FALTA
+#########################################################################################################################
+class Falta(models.Model):
+    nomeFalta = models.CharField(max_length=250)
+    penalidade = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nomeFalta
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA FALTA COMETIDA
+#########################################################################################################################
+class FaltaCometida(models.Model):
+    conclusaoJogada = models.ForeignKey("ConclusaoJogada", on_delete=models.CASCADE)
+    tipoFalta = models.ForeignKey(Falta, on_delete=models.CASCADE)
+    timeCometeuFalta = models.ForeignKey(Time, on_delete=models.CASCADE)
+    num_jogadorFezFalta = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.tipoFalta} cometida pelo jogador {self.num_jogadorFezFalta}"
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA PONTUACAO
+#########################################################################################################################
+class Pontuacao(models.Model):
+    nomePontuacao = models.CharField(max_length=25)
+    descPontuacao = models.CharField(max_length=2000)
+    qtsPonto = models.IntegerField()
+
+    def __str__(self):
+        return self.nomePontuacao
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA CONCLUSAO JOGADA
+#########################################################################################################################
+class ConclusaoJogada(models.Model):
+    jogada = models.ForeignKey(Jogada, on_delete=models.CASCADE)
+    tipoJogada = models.CharField(max_length=25)
+    possuiFalta = models.BooleanField(default=False)
+    jogPossuiFumble = models.BooleanField(default=False)
+    timeAtaqueRecuperou = models.BooleanField(default=False)
+    jogadaComPontuacao = models.BooleanField(default=False)
+    tipoPontuacao = models.ForeignKey(Pontuacao, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"Conclusão da Jogada {self.jogada.id}"
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA PASSE
+#########################################################################################################################
+class Passe(models.Model):
+    conclusaoJogada = models.ForeignKey(ConclusaoJogada, on_delete=models.CASCADE)
+    n_quaterback = models.IntegerField()
+    n_recebedor = models.IntegerField()
+    resultado_jogada = models.CharField(max_length=50)
+    direcao_passe = models.CharField(max_length=50)
+    qt_jard_percorreu = models.IntegerField()
+    profundidade_passe = models.CharField(max_length=50)
+    def_parou_jogada = models.BooleanField(default=False)
+    jogador_parou_jogada = models.IntegerField()
+
+    def __str__(self):
+        return f"Passe do QB {self.n_quaterback}"
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA CORRIDA
+#########################################################################################################################
+class Corrida(models.Model):
+    conclusaoJogada = models.ForeignKey(ConclusaoJogada, on_delete=models.CASCADE)
+    n_quaterback = models.IntegerField()
+    n_corredor = models.IntegerField()
+    resultado_jogada = models.CharField(max_length=50)
+    direcao_corrida = models.CharField(max_length=50)
+    qt_jard_percorreu = models.IntegerField()
+    gap_corrida = models.CharField(max_length=10)
+    def_parou_jogada = models.BooleanField(default=False)
+    jogador_parou_jogada = models.IntegerField()
+
+    def __str__(self):
+        return f"Corrida do jogador {self.n_corredor}"
+
+#########################################################################################################################
+#SEGUE BASE MODEL DA TABELA PUNT
+#########################################################################################################################
+class Punt(models.Model):
+    conclusaoJogada = models.ForeignKey(ConclusaoJogada, on_delete=models.CASCADE)
+    n_kicker = models.IntegerField()
+    resultado_jogada = models.CharField(max_length=50)
+    posicao_campo = models.IntegerField()
+    qt_jard_punt = models.IntegerField()
+    teve_retorno = models.BooleanField(default=False)
+    n_jogadorRetorno = models.IntegerField()
+    qt_jard_retorno = models.IntegerField()
+    def_parou_jogada = models.BooleanField(default=False)
+    jogador_parou_jogada = models.IntegerField()
+
+    def __str__(self):
+        return f"Punt do jogador {self.n_kicker}"
+
+#####################################################################################################################
+#SEGUE BASE MODEL DA TABELA FIELD GOAL
+######################################################################################################################
+class FieldGoal(models.Model):
+    conclusaoJogada = models.ForeignKey(ConclusaoJogada, on_delete=models.CASCADE)
+    n_kicker = models.IntegerField()
+    resultado_jogada = models.CharField(max_length=50)
+    posicao_campo = models.IntegerField()
+    def_parou_jogada = models.BooleanField(default=False)
+    jogador_parou_jogada = models.IntegerField()
+
+    def __str__(self):
+        return f"Field Goal do kicker {self.n_kicker}"
